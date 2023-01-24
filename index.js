8aedd0b92a0952fcdf57957e23c7361b16312c6c
vaishnavi-mtalkz <vaishnavi.mtalkz@gmail.com>
2023-01-24 10:55:25 +0530

    strings and functions

--- index.js
+++ index.js
@@ -36,6 +36,55 @@
 // console.log(str1);
 // console.log(str2);
 
+//2
+//  let str="vaishu"
+//  console.log(typeof(str))
 
-let str="vaishu"
-console.log(typeof(str))
+
+//3
+// let arr=[1,2,3,4,5]
+// arr.splice(0,arr.length)
+// console.log(arr)
+
+
+
+//4
+// let arr=[1,2,3,4,5]
+// let sum=0
+// for(let i=0;i<arr.length;i++)
+// {
+//   sum+=arr[i]
+// }
+// console.log(sum)
+
+
+//5
+// let arr=[1,2,3,4,5]
+// let arr1=[]
+// for(i=0;i<arr.length;i++)
+// {
+// arr1[i]=arr[i]
+// }
+// console.log(arr1)
+
+//6
+//without duplication 
+// let arr=[1,2,3,4,5,6]
+// let arr1=[7,8,9,10]
+// console.log(arr.concat(arr1))
+
+//with duplication
+//6
+// let arr=[1,2,3,4,5,6]
+// let arr1=[5,6,7]
+
+// for(let i=0;i<arr1.length;i++){
+//  if(!arr.includes(arr1[i])){
+//    arr.push(arr1[i])
+//  }
+
+// }
+// console.log(arr)
+
+
+
