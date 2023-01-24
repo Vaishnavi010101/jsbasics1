9a07d308becf10d5a1002f4a46ae17682f128bc5
vaishnavi-mtalkz <vaishnavi.mtalkz@gmail.com>
2023-01-24 07:50:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// function greet(name){
+// console.log('hello'+name);
+// }
+// greet("john");
+
+//let marks={
+   // harry:90,
+    //shubham:90,
+    //lovish:56,
+    //monika:4
+//}
+//for(let i in marks )
+//{
+  //  console.log("THE marks of" + i + "are" +marks[i]);
+//}
+//const mean=(a,b,c,d)=>{ 
+  //  console.log (typeof a)
+    //return ((a+b+c+d)/4)
+//}
+//console.log(mean(1,2,3,4));
+//let name="vaishnavi"
+//console.log(name.slice(6));
+//let name1="vaishnavi"
+//let name2='garg'
+//console.log(name1.concat(name2,"yes "))
+//let str="vaishnavi"
+//let str1=""
+//let str2=""
+//for(i=0;i<str.length;i++)
+// {
+//     if(i%2==0)
+//     str1+=str[i]
+//     else
+//     str2+=str[i]
+//     }
+// console.log(str1);
+// console.log(str2);
+
+
+let str="vaishu"
+console.log(typeof(str))
